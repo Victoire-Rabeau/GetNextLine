7836177cceb5237a66f0bbe04671704b139f8d0f
Victoire Rabeau <vrabeau@localhost.localdomain>
2020-11-28 19:47:49 +0100

    [ADD] project

---
+++ get_next_line.c
@@ -0,0 +1,88 @@
+/*
+** EPITECH PROJECT, 2020
+** get next line
+** File description:
+** main
+*/
+
+#include "get_next_line.h"
+
+int my_line_len(char const *str)
+{
+    int i = 0;
+
+    for (; str[i] && str[i] != '\n'; i++);
+    return i;
+}
+
+char *read_file(int fd, char *buff)
+{
+    int offset = 0;
+    int len = 1;
+
+    while (len > 0) {
+        len = read(fd, buff + offset, READ_SIZE);
+        offset += len;
+    }
+    buff[offset] = '\0';
+    return buff;
+}
+
+char *string_to_array(char *buff, int len)
+{
+    char *str = malloc(sizeof(char) * len + 1);
+    int i = 0;
+    int j = 0;
+
+    if (!str)
+        return NULL;
+    for (; j < len ; j++) {
+        str[j] = buff[i];
+        i++;
+    }
+    str[len] = '\0';
+    return str;
+}
+
+char **fill_buffer(char *buff)
+{
+    char **buffer;
+    int y = 1;
+    int j = 0;
+    int len = 0;
+    int count = 1;
+
+    for (int i = 0; buff[i]; i++)
+        if (buff[i] == '\n')
+            y++;
+    buffer = malloc(sizeof(char *) * (y + 1));
+    if (!buffer)
+        return NULL;
+    for (; count <= y; count++, j++) {
+        len = my_line_len(buff);
+        buffer[j] = malloc(sizeof(char) * len + 1);
+        buffer[j] = string_to_array(buff, len);
+        buff += (len + 1);
+    }
+    buffer[j] = NULL;
+    return buffer;
+}
+
+char *get_next_line(int fd)
+{
+    char *buff;
+    char buff2[1000000];
+    static char **buffer;
+    static int i = -1;
+
+    if (fd == -1)
+        return NULL;
+    if (i == -1) {
+        buff = read_file(fd, buff2);
+        buffer = fill_buffer(buff);
+    }
+    if (!buffer)
+        return NULL;
+    i++;
+    return buffer[i];
+}
---
+++ get_next_line.h
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2020
+** get next line
+** File description:
+** header
+*/
+
+#include <unistd.h>
+#include <stddef.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#ifndef READ_SIZE
+#define READ_SIZE (10)
+
+#endif /* !READ_SIZE */
